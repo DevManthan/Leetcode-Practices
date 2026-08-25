76262ba1a536ea5b1e465f931ded860c7bb8c991
DevManthan <manthandhiman007@gmail.com>
2026-08-25 23:08:08 +0530

    Time: 1 ms (82.00%) | Memory: 45 MB (71.22%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        
+        int j = 1;
+        
+        while(true){
+        int target = k*j;
+        boolean found = false;
+            for(int i = 0;i<nums.length;i++){
+                if(nums[i] == k*j){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                return target;
+            }
+            j++;
+
+        }
+    }
+}
