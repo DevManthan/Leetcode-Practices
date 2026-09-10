9a92ca412a1cb9c73f8c855062f325f008759245
Manthan Dhiman <manthandhiman007@gmail.com>
2026-09-10 22:58:16 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (14.09%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -2,19 +2,20 @@ class Solution {
     public int diagonalSum(int[][] mat) {
         int sum = 0;
         int n = mat.length;
-        for(int i = 0;i < n;i++){
-            for(int j = 0; j < n;j++){
-                if(i == j){
-                    sum += mat[i][j];
-                }
-                if(i + j == n-1){
-                    sum += mat[i][j];
-                }
-            }
+
+        for (int i = 0; i < n; i++) {
+            // Primary diagonal
+            sum += mat[i][i];
+            
+            // Secondary diagonal
+            sum += mat[i][n - 1 - i];
         }
-        if(n % 2 != 0){
-            sum -= mat[n/2][n/2];
+
+        // Agar odd dimension hai, center element do baar jud chuka hai
+        if (n % 2 != 0) {
+            sum -= mat[n / 2][n / 2];
         }
+
         return sum;
     }
 }
