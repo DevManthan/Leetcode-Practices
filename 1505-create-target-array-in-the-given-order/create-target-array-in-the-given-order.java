35946ae128481e6fd37e89477465448455d43317
Manthan Dhiman <manthandhiman007@gmail.com>
2026-09-10 22:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (25.12%) - LeetSync

--- 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.java
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        ArrayList<Integer> list = new ArrayList();
+        int n = nums.length;
+        int[] target = new int[n];
 
-        for (int i = 0; i < nums.length; i++) {
-            list.add(index[i], nums[i]);
-        }
-
-        int[] target = new int[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            target[i] = list.get(i);
+        for (int i = 0; i < n; i++) {
+            int idx = index[i];
+            // Agar index current filled boundary se pehle hai, toh elements right shift karo
+            if (idx < i) {
+                System.arraycopy(target, idx, target, idx + 1, i - idx);
+            }
+            target[idx] = nums[i];
         }
 
         return target;
