484c75371df1d19b11dce422c126ee734d2732fd
Manthan Dhiman <manthandhiman007@gmail.com>
2026-09-11 22:53:01 +0530

    Time: 55 ms (18.51%) | Memory: 47.7 MB (49.41%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> result = new ArrayList<>();
+        int i = num.length - 1;
+
+        // Loop as long as there are digits in 'num' OR 'k' has a value left to add
+        while (i >= 0 || k > 0) {
+            // Add the current digit of num to k if we haven't run out of digits
+            if (i >= 0) {
+                k += num[i];
+                i--;
+            }
+            
+            // The last digit of k is the value for the current position
+            result.add(0,k % 10);
+            
+            // Divide k by 10 to carry over the remaining value to the next position
+            k /= 10;
+        }
+
+        // Since we added digits from right to left, reverse the list to get the correct order
+        // Collections.reverse(result);
+        return result;
+    }
+}
