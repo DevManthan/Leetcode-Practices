67938dcb06c7d85d6408f4627a623cf34c03985e
Manthan Dhiman <manthandhiman007@gmail.com>
2026-09-12 22:27:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (95.21%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.py
+++ 1983-maximum-population-year/maximum-population-year.py
@@ -1,17 +1,20 @@
 class Solution(object):
     def maximumPopulation(self, logs):
-        max_population = 0
+        year = [0] * 101
+
+        for birth, death in logs:
+            year[birth - 1950] += 1
+            year[death - 1950] -= 1
+
         max_year = 1950
+        population = 0
+        max_population = 0
 
-        for year in range(1950, 2051):
-            population = 0
-
-            for birth, death in logs:
-                if birth <= year < death:
-                    population += 1
+        for i in range(0, 101):
+            population += year[i]
 
             if population > max_population:
                 max_population = population
-                max_year = year
+                max_year = i + 1950
 
         return max_year
