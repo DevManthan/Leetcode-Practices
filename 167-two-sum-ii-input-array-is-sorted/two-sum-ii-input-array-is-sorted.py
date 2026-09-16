ab053387480c93cbdd9a38fd89ecc5d1701d1f6b
Manthan Dhiman <manthandhiman007@gmail.com>
2026-09-16 22:52:07 +0530

    Time: 1 ms (92.52%) | Memory: 14.6 MB (5.07%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -3,6 +3,8 @@ class Solution(object):
         seen={} # {number:index}
         for i,num in enumerate(numbers):
             complement=target-num  
+
             if complement in seen:
                 return [seen[complement]+1,i+1]
+                
             seen[num]=i
