a5cc2ce950daf4eda23fe1f18e0d5061626a9f87
Manthan Dhiman <manthandhiman007@gmail.com>
2026-09-23 22:35:09 +0530

    Time: 9 ms (58.81%) | Memory: 48 MB (91.58%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.java
+++ 924-fair-candy-swap/fair-candy-swap.java
@@ -1,22 +1,27 @@
 class Solution {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
-        int aSum = sum(aliceSizes);
-        int bSum = sum(bobSizes);
-        Arrays.sort(bobSizes);
-        int mid = aSum+(bSum-aSum)/2;
-        for(int i: aliceSizes){
-            int a = Arrays.binarySearch(bobSizes,(bSum+i)-mid);
-            if(a >= 0){
-                return new int[]{i,bobSizes[a]};
+        int[] ans = new int[2];
+        int aSum = 0;
+        int bSum = 0;
+        for(int x: aliceSizes){
+            aSum += x;
+        }
+        for(int x: bobSizes){
+            bSum += x;
+        }
+
+        int target = (aSum + bSum)/2;
+        int diff = target - aSum;
+        Set<Integer> set = new HashSet<>();
+        for (int b : bobSizes) set.add(b);
+        for(int x : aliceSizes){
+            if(set.contains(x+diff)){
+                return new int[]{x,x+diff};
             }
         }
-        return new int[]{-1,-1};
+
+        return new int[2];
+       
     }
-    public int sum(int[] ar){
-        int s = 0;
-        for(int i: ar){
-            s += i;
-        }
-        return s;
-    }
+
 }
